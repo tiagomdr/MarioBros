cfc64174781fc640a3b815fa5b9b92f241e337ed
tiagomdr <tiago335tm@gmail.com>
2025-02-14 19:39:22 -0300

    Projeto-Mario

---
+++ script.js
@@ -0,0 +1,32 @@
+const mascara = document.querySelector(".mascara-formulario")
+const form = document.querySelector('.formulario')
+
+function contato() {
+
+    form.style.left = "10%"
+    form.style.transform = "translateX(-20)"
+    mascara.style.visibility = "visible"
+
+    
+
+
+}
+
+function mostrarFormulario() {
+
+    form.style.left = "10%"
+    form.style.transform = "translateX(-20)"
+    mascara.style.visibility = "visible"
+
+    
+
+
+}
+
+
+function desaparecer() {
+
+    mascara.style.visibility = "hidden"
+    form.style.left = "-280px"
+
+}
